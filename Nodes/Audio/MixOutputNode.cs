aaa4257e23f195f89ea7ca8768185916b7a96bd5
MuffinTastic <muffintastic32@gmail.com>
2022-05-15 12:03:05 -0700

    rename mix output node

--- Nodes/Audio/MixOutputNode.cs
+++ Nodes/Audio/MixOutputNode.cs
@@ -4,8 +4,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SandMix.Nodes.Audio;
 
-[Library, Display( Name = "Output", Description = "Audio output - corresponds to a single ingame sound stream", GroupName = "Audio" )]
-public class OutputNode : BaseNode
+[Library, Display( Name = "Mix Output", Description = "Plays the output of the mix as a single ingame sound stream", GroupName = "Audio" )]
+public class MixOutputNode : BaseNode
 {
 	[Browsable( false ), Input]
 	public Types.Audio Output { get; set; }
