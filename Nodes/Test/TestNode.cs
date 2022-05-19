00d721ac26d66d18b1d0e0be3460cb9c9730f012
MuffinTastic <muffintastic32@gmail.com>
2022-05-19 12:46:43 -0700

    test node, will delete later

---
+++ Nodes/Test/TestNode.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Sandbox;
+
+namespace SandMix.Nodes.Test;
+
+[Library, Display( Name = "Test", GroupName = "Test" )]
+public class TestNode : BaseNode
+{
+	[Output, ResourceType("folder")]
+	public string Testo { get; set; }
+
+	[Output]
+	public float AAWD { get; set; }
+}
