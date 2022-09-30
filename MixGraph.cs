a12f69cc9fcadd9818e9fedbea50b5304ebec4cf
MuffinTastic <muffintastic32@gmail.com>
2022-09-29 20:18:34 -0700

    move MixGraph from testing env, for later use

---
+++ MixGraph.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox;
+using SandMix.Nodes;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SandMix;
+
+public class MixGraph
+{
+	public string File { get; private set; }
+
+	private FileWatch GraphWatch;
+	private GraphContainer Graph;
+
+	public MixGraph( string file )
+	{
+		File = file;
+
+		ReloadGraph();
+	}
+
+	private void ReloadGraph()
+	{
+		var data = FileSystem.Mounted.ReadAllText( File );
+
+		try
+		{
+			Graph = GraphContainer.Deserialize( data );
+		}
+		catch ( JsonException ex )
+		{
+			Log.Error( $"Couldn't deserialize mixgraph {File}: {ex.Message}" );
+		}
+
+		Log.Info( $"Mixgraph {File} loaded" );
+	}
+}
--- Nodes/BaseNodeConverter.cs
+++ Nodes/BaseNodeConverter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Sandbox;
 
 namespace SandMix.Nodes;
 
